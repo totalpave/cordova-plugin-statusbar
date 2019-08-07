07bf121bd152c1a5e29b5724c78e2a285366a080
Tyler Breau <tyler.breau@totalpave.com>
2019-08-07 15:38:08 -0300

    Revert "I think overlayswebview is returning before the ui actually updates. I'm trying to call callbackcontext after the UI updates by using event listeners."
    This reverts commit f50f19d43f7a49cd0b96e8d5a652badcf38f9fc5.

--- src/android/StatusBar.java
+++ src/android/StatusBar.java
@@ -181,7 +181,8 @@ public class StatusBar extends CordovaPlugin {
                     @Override
                     public void run() {
                         try {
-                            setStatusBarTransparent(args.getBoolean(0), callbackContext);
+                            setStatusBarTransparent(args.getBoolean(0));
+                            callbackContext.success();
                         } catch (JSONException ignore) {
                             LOG.e(TAG, "Invalid boolean argument");
                             callbackContext.error("Invalid boolean argument");
@@ -256,17 +257,9 @@ public class StatusBar extends CordovaPlugin {
         }
     }
 
-    private void setStatusBarTransparent(final boolean transparent, final CallbackContext callbackContext) {
+    private void setStatusBarTransparent(final boolean transparent) {
         if (Build.VERSION.SDK_INT >= 21) {
             final Window window = cordova.getActivity().getWindow();
-            if(callbackContext) {
-            window.getDecorView().setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
-                    @Override
-                    public void onSystemUiVisibilityChange(int visibility) {
-                        callbackContext.success();
-                    }
-                });
-            }
             if (transparent) {
                 window.getDecorView().setSystemUiVisibility(
                         View.SYSTEM_UI_FLAG_LAYOUT_STABLE
